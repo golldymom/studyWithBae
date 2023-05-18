380632caa04ad43ead9f2fff22d9b8a60bae8eb9
golldymom <86226500+golldymom@users.noreply.github.com>
2023-05-18 17:51:56 +0900

    [unrated] Title: 정수 찾기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.js
@@ -0,0 +1,11 @@
+function solution(num_list, n) {
+    var answer = 0;
+    if(num_list.includes(n)===true){
+        answer = 1
+        return answer
+    }else{
+        answer = 0
+        return answer
+    }
+    return answer;
+}
