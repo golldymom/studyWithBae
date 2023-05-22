b1d4f7909b72c0b7bd97d35fe2296b1191ef100d
golldymom <86226500+golldymom@users.noreply.github.com>
2023-05-22 09:42:28 +0900

    [unrated] Title: 부분 문자열인지 확인하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.js
@@ -0,0 +1,14 @@
+function solution(my_string, target) {
+    var answer = 0;
+    // console.log(my_string.includes(target))
+    if(my_string.includes(target) === true){
+        answer = 1;
+        // console.log(answer);
+        return answer;
+    }else{
+        answer = 0;
+        // console.log(answer);
+        return answer;
+    }
+
+}
