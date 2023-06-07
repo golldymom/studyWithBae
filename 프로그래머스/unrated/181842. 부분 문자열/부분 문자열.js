574d8be78ab13de4c5cde807efdf3081b6241141
golldymom <86226500+golldymom@users.noreply.github.com>
2023-06-07 10:13:57 +0900

    [unrated] Title: 부분 문자열, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.js
@@ -0,0 +1,8 @@
+function solution(str1, str2) {
+    var answer = '';
+    str2.includes(str1) === true ? answer = 1 : answer = 0;
+    // if(str2.includes(str1) === true){
+    //     answer = 1
+    // }
+ return answer
+}
