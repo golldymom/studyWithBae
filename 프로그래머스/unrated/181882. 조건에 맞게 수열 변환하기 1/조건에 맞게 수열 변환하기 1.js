a084ed7c1e9db6a70d3bf2d157639e59dda6c986
golldymom <86226500+golldymom@users.noreply.github.com>
2023-06-08 10:49:01 +0900

    Update 조건에 맞게 수열 변환하기 1.js

--- 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -1,8 +1,12 @@
 function solution(arr) {
-    var answer = arr.map((a)=> {
-        if(a >=50 && a%2 === 0) {return  a /2} 
-        else if(a < 50 && a %2 !== 0) {return a* 2}
-        else {return a}
-                               });
+//     var answer = arr.map((a)=> {
+//         if(a >=50 && a%2 === 0) {return  a /2} 
+//         else if(a < 50 && a %2 !== 0) {return a* 2}
+//         else {return a}
+//                                });
+//     map 과 삼항연산자 사용하기
+     var answer = arr.map((a)=> {
+     return (a >= 50 && a % 2 === 0) ? a / 2 : (a < 50 && a % 2 !== 0) ? a * 2 : a
+            });
     return answer;
 }
