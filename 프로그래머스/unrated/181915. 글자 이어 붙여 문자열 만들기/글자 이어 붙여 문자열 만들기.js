5e097d8e99917a957b4f7f3c0c10b0f695f9f173
golldymom <86226500+golldymom@users.noreply.github.com>
2023-06-11 22:34:52 +0900

    [unrated] Title: 글자 이어 붙여 문자열 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
+++ 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
@@ -1,6 +1,8 @@
 function solution(my_string, index_list) {
     // let arr = my_string.split('')
     // console.log(arr)
-    var answer = index_list.reduce((acc, cur) => acc+my_string[cur], "");
+    // var answer = index_list.reduce((acc, cur) => console.log(my_string[cur]),acc+my_string[cur], "");
+     var answer = index_list.map((index) => my_string[index]).join("");
+  
     return answer;
 }
