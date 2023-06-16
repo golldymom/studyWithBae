054c98f28d3991f9099788f4ca8d78de03326ee6
golldymom <86226500+golldymom@users.noreply.github.com>
2023-06-16 23:58:21 +0900

    Update 배열의 원소만큼 추가하기.js

--- 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.js
+++ 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.js
@@ -7,7 +7,7 @@ function solution(arr) {
     }
   }
   return answer;
-    // var answer = arr.map((a,b) =>  b.repeat(a),'');
+    // var answer = arr.map((a,b) =>  b.repeat(a),''); //
     // var answer = arr.flatMap((element) => String(element).repeat(element).split("").map(Number));
     
   //   var answer = arr.reduce((acc, cur) => {
