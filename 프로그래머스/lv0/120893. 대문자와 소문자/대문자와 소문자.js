81e9466b1e1568b7fd85898354d4261fe26f5a4b
golldymom <86226500+golldymom@users.noreply.github.com>
2023-06-21 12:00:05 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.53 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,15 +1,19 @@
 function solution(my_string) {
     // let map = {
-    //     toLowerCase() : toUpperCase()
-    //     toUpperCase() : toLowerCase()
+    //    'a': 'A',
+    // 'A': 'a',
+    //     _,
+    //     'z': 'Z',
+    //     'Z': 'z',
     // }
     // var answer = [...my_string].map(a => mapping[a]).join('');
-    var answer = [...my_string].map(char => {
-    if (char === char.toUpperCase()) {
-      return char.toLowerCase();
-    } else {
-      return char.toUpperCase();
-    }
-  }).join('');
+  //   var answer = [...my_string].map(char => {
+  //   if (char === char.toUpperCase()) {
+  //     return char.toLowerCase();
+  //   } else {
+  //     return char.toUpperCase();
+  //   }
+  // }).join('');
+   var answer = my_string.replace(/(\w)/g,c=>/[a-z]/.test(c)?c.toUpperCase():c.toLowerCase())
     return answer;
 }
