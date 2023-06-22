c463ad72746fab46c52a2b2e1579c91fc92b4787
golldymom <86226500+golldymom@users.noreply.github.com>
2023-06-22 14:08:32 +0900

    [level 0] Title: 주사위의 개수, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
@@ -0,0 +1,11 @@
+function solution(box, n) {
+    // let ga = parseInt(box[0]/n)
+    // let se = parseInt(box[1]/n)
+    // let no = parseInt(box[2]/n)
+    // console.log(ga,se,no)
+    // var answer = ga*se*no;
+    // var answer = Math.floor(box[0] / n) * Math.floor(box[1] / n) * Math.floor(box[2] / n);
+    var answer = parseInt(box[0]/n)*parseInt(box[1]/n)*parseInt(box[2]/n)
+    return answer;
+//     직육면체 부피 : a*b*c
+}
