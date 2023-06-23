d8547f4f04ca34f2844b59eec5f235dd4b8ce88e
golldymom <86226500+golldymom@users.noreply.github.com>
2023-06-23 11:00:44 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.js
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -1,5 +1,6 @@
 function solution(n, numlist) {
     var answer = [];
-   numlist.reduce((acc, t) => {if(t % n === 0){answer.push(t)}}, 0 )
+   // numlist.reduce((acc, t) => {if(t % n === 0){answer.push(t)}}, 0 )
+    numlist.reduce((acc, t) => {t % n === 0 ? answer.push(t): acc}, 0 )
     return answer;
 }
