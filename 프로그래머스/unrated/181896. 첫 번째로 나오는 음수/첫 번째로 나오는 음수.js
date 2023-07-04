2910e07e3db036877f5c1e1e7e3293eb8b64f9b7
golldymom <86226500+golldymom@users.noreply.github.com>
2023-07-04 20:31:49 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,15 +1,17 @@
 function solution(num_list) {
-    // var answer = num_list.find((v)=> v <0) ? num_list.indexOf((v)=>v<0) : -1;
+    var answer = num_list.find((v)=> v <0) ? num_list.findIndex((v)=>v<0) : -1;
     // var answer = num_list.indexOf((v)=> v<0)
-    var answer = num_list.findIndex((v)=> v <0 )
+    // var answer = num_list.findIndex((v)=> v <0 )
     // console.log(num_list.find((v)=> v === parseInt(v)*-1))
-    // for(let n=0; n<=num_list.length; n++){
+    // for(let n=0; n<num_list.length; n++){
     //     if(num_list[n] < 0){
-    //         answer = num_list.indexOf(n)
-    //         console.log('here')
+    //         // answer = num_list.indexOf(n)
+    //         answer = n
+    //         return n
+    //         break;
     //     } else{
     //         answer = -1
-    //         console.log('else')
+    //         break;
     //     }
     //      // return answer;
     // }
