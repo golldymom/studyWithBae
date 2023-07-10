5f6538a70ca198072438d2d8389a52aa9d89df04
golldymom <86226500+golldymom@users.noreply.github.com>
2023-07-10 14:54:22 +0900

    [unrated] Title: 꼬리 문자열, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.js
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.js
@@ -1,12 +1,16 @@
 function solution(str_list, ex) {
-    var answer = str_list.filter((e)=> !e.includes(ex)).join("")
+    // var answer = str_list.filter((e)=> !e.includes(ex)).join("")
+    var answer = [];
     // var reg = /ex/g
-    // var answer = str_list.map((char,i)=> (char[i].includes(ex) != -1) ? answer.push(char) : "")
+    // var answer = 
+       let test= str_list.map((char)=> (!char.includes(ex)) ? answer.push(char) : "")
+      let result= answer.join("")
     // let test1 = str_list.filter((e)=> !e.includes(ex)).join("")
     // console.log(test1)
 //     for(let i =0; i<=str_list.length;i++){
 //         console.log(str_list.filter((e)=> e)
         
 // //     }
-    return answer;
+    // return answer;
+      return result
 }
