2fbb4f5419d943c7167c951fde2bba6202f26530
golldymom <86226500+golldymom@users.noreply.github.com>
2023-07-15 21:34:06 +0900

    [unrated] Title: 카운트 다운, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.js
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.js
@@ -5,9 +5,16 @@ function solution(start, end) {
     //     console.log(i)
     // }
     // return answer;
-    var answer = Array.from({length:start-end+1},((v,i)=>i+end)).sort((a,b)=>b-a)
-    // let m =Array.from({length:start-end+1},((v,i)=>i+1))
-    // var answer = m.filter((i)=> i%k ===0);
-
+    // var answer = Array.from({length:start-end+1},((v,i)=>i+end)).sort((a,b)=>b-a)
+    // var answer = Array.from({length:start-end+1},((v,i)=>i-1)) //[-1, 0, 1, 2, 3, 4, 5, 6]
+    var answer = Array.from({length:start-end+1},((v,i)=>start - i)) //성공
+    // var answer = Array.from(Array(start - end + 1), (_, i) => start - i);
+ // for(let i = end; i <= start; i++){
+ //        answer.unshift(i)
+ //    }
+    //  for(let i = start; i>=end; i--){
+    //     answer.push(i)
+    // }
+// var answer = Array(start-end+1).fill(start).map((v,i)=>v-i);
     return answer;
 }
