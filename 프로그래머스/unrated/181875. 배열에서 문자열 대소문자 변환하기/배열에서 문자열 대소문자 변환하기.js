74b262fa788b9c7ec0ec1f8198d90717a347aef5
golldymom <86226500+golldymom@users.noreply.github.com>
2023-07-18 16:35:27 +0900

    [unrated] Title: 배열에서 문자열 대소문자 변환하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.js
+++ 프로그래머스/unrated/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.js
@@ -1,10 +1,12 @@
 function solution(strArr) {
-    var answer = [];
+    // var answer = [];
     // var answer = strArr.reduce((a,c,i)=>(!i%2)? [...a, c.toLowerCase()]:[a+c.toUpperCase()],[])
     // var answer = strArr.reduce((a,c,i)=> i%2===0 ? a+[ c.toLowerCase()] : a,'')  //reduce는 한개의 문자열로 합치는 성질이 있음.
     // var answer = 
-        strArr.map((a,i) => i%2 ? answer.push(a.toUpperCase()) : answer.push(a.toLowerCase()))
+        // strArr.map((a,i) => i%2 ? answer.push(a.toUpperCase()) : answer.push(a.toLowerCase()))
+     var answer = strArr.reduce((a, c, i) => {
+    return a.concat(i % 2 ? c.toUpperCase() : c.toLowerCase());
+  }, []);
     
     return answer;
 }
- // [...result, index % 2 === 1 ? num + n : num]
