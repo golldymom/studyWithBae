3afcbc9dd5661a5faec1e2521c5105afff978bd8
golldymom <86226500+golldymom@users.noreply.github.com>
2023-07-21 11:27:09 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
@@ -1,6 +1,7 @@
 function solution(my_string) {
     // var answer = my_string.split(" "); // ["","i","","","","love","","you"]
     // var answer = my_string.split(" ").filter(a=> (a===" "))
-    var answer = my_string.split(" ").filter(a=> !a== "")
+    // var answer = my_string.split(" ").filter(a=> !a== "")
+    var answer = my_string.split(" ").filter(a => a)
     return answer;
 }
