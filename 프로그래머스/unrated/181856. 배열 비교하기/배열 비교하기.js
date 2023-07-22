a9f8b26b92697c554c009227883a80c289925401
golldymom <86226500+golldymom@users.noreply.github.com>
2023-07-22 23:39:31 +0900

    [unrated] Title: 배열 비교하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.js
@@ -0,0 +1,20 @@
+function solution(arr1, arr2) {
+    var answer = 0 ;
+    // if(arr1.length > arr2.length){
+    //     return answer = 1
+    // }else if(arr1.length< arr2.length){
+    //     return answer = -1
+    // }else if(arr1.length === arr2.length){
+    //     if(arr1.reduce((a,b)=>a+b,0) > arr2.reduce((a,b)=>a+b,0)){
+    //        return answer = 1
+    //        }else if(arr1.reduce((a,b)=>a+b,0)< arr2.reduce((a,b)=>a+b,0)){
+    //            return answer = 1
+    //        } else  {return answer = 0}
+    // }
+    if(arr1.length != arr2.length){
+        arr1.length > arr2.length ? answer = 1 : answer = -1
+    } else {
+        arr1.reduce((a,b)=>a+b,0) > arr2.reduce((a,b)=>a+b,0) ? answer = 1 : arr1.reduce((a,b)=>a+b,0) == arr2.reduce((a,b)=>a+b,0)? answer = 0 : answer = -1
+    }
+    return answer;
+}
