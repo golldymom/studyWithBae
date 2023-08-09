f2c9cd3cc3435a5030dfe3460913de9e059daa33
golldymom <86226500+golldymom@users.noreply.github.com>
2023-08-09 22:53:58 +0900

    [unrated] Title: 9로 나눈 나머지, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.js
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.js
@@ -1,5 +1,6 @@
 function solution(number) {
-    var mp = [...number].reduce((a,b)=>+a+ +b,'');
-    var answer = mp%9
+    // var mp = [...number].reduce((a,b)=>+a+ +b,'');
+    // var answer = mp%9
+var answer = [...number].reduce((a,b)=>+a+ +b,'')%9
     return answer;
 }
