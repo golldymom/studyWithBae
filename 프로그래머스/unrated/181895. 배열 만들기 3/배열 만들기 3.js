800bd27958aa808e23f5db436d5c5bc4dfacfd83
golldymom <86226500+golldymom@users.noreply.github.com>
2023-08-14 22:32:57 +0900

    [unrated] Title: 배열 만들기 3, Time: 0.10 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.js
@@ -6,9 +6,13 @@ function solution(arr, intervals) {
     // var fi2 = fi[1]
     // var se1 = se[0]
     // var se2 = se[1]
-    var  [[fi1,fi2],[se1,se2]] = intervals;
+
     // console.log(fi,se)
-    console.log(fi1,fi2)
-    answer.push(arr.slice(fi1,fi2+1).concat(arr.slice(se1,se2+1)))
-    return answer[0];
+    // console.log(fi1,fi2)
+    // answer.push(arr.slice(fi1,fi2+1).concat(arr.slice(se1,se2+1)))
+        // return answer[0];
+        var  [[fi1,fi2],[se1,se2]] = intervals;
+    answer = arr.slice(fi1,fi2+1).concat(arr.slice(se1,se2+1))
+    return answer
+
 }
