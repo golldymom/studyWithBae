cba3228924cf055c539feed247493b009a4afa61
golldymom <86226500+golldymom@users.noreply.github.com>
2023-08-15 16:27:47 +0900

    [unrated] Title: 두 수의 연산값 비교하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
+++ 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
@@ -1,5 +1,6 @@
 function solution(a, b) {
-    var answer = Math.max(String(a)+String(b), 2*a*b);
+    // var answer = Math.max(String(a)+String(b), 2*a*b);
+    var answer = Math.max(a+""+b+"", 2*a*b);
     // console.log(String(a)+String(b), 2*a*b)
     return answer;
 }
