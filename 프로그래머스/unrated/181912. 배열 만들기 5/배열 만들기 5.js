546805675fcc9aef78a1f42f14680fc9c163a063
golldymom <86226500+golldymom@users.noreply.github.com>
2023-08-16 23:35:43 +0900

    [unrated] Title: 배열 만들기 5, Time: 1.40 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.js
@@ -0,0 +1,20 @@
+function solution(intStrs, k, s, l) {
+    var answer = [];
+    // var newIntStrs  = intStrs
+    // var test1,test2,test3 = newIntStrs.slice(0,1)
+    // var testt = newIntStrs.filter(a=>a.slice(s,1)>k)
+// answer = intStrs.filter(str => {
+//         const subStr = str.slice(s, s+l);
+//         const num = parseInt(subStr);
+//         return num > k;
+//     }).map(str => parseInt(str));
+     answer = intStrs.map(str => {
+        const subStr = str.substring(s, s + l);
+        const num = parseInt(subStr);
+        return num;
+    }).filter(num => num > k);
+    // console.log(testt)
+    // console.log(typeof(s))
+    
+    return answer;
+}
