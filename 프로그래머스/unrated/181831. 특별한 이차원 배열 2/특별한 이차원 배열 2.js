30d50f38cc587fdc3d995eafcc3467cc669782a2
golldymom <86226500+golldymom@users.noreply.github.com>
2023-08-22 23:41:50 +0900

    [unrated] Title: 특별한 이차원 배열 2, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -0,0 +1,18 @@
+function solution(arr) {
+    var answer =1;
+    // for(let i,j  = 1; i,j<=arr.length;i,j++){
+    //     if(arr[i][j]==arr[j][i]){
+    //         answer = 1
+    //     }else{
+    //         answer =0
+    //     }
+    // }
+    for(let i=0; i<arr.length; i++){
+        for(let j = 0; j<arr.length; j++){
+            if(arr[i][j]!=arr[j][i]){
+                answer = 0
+            }
+        }
+    }
+    return answer;
+}
