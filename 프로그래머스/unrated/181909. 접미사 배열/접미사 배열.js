432588cad9f0cf46e0a5f97602aa1d99b2c666a4
golldymom <86226500+golldymom@users.noreply.github.com>
2023-08-24 23:12:12 +0900

    [unrated] Title: 접미사 배열, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.js
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.js
@@ -1,10 +1,13 @@
 function solution(my_string) {
     var answer = [];
-     const strLen = my_string.length
+//      const strLen = my_string.length
      
-    answer= [...my_string].reduce((acc, cur, idx) => {
-        const curStr = my_string.slice(idx, strLen)
-        return [...acc, curStr]
-    }, []).sort()
+//     answer= [...my_string].reduce((acc, cur, idx) => {
+//         const curStr = my_string.slice(idx, strLen)
+//         return [...acc, curStr]
+//     }, []).sort()
+    answer = Array.from(my_string)
+    .map((_, i) => my_string.substring(i))
+    .sort();
     return answer;
 }
