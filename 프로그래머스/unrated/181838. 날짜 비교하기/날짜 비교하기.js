4cca6165b7f8c0830deea7ae7d43e362f933c192
golldymom <86226500+golldymom@users.noreply.github.com>
2023-08-27 22:43:05 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,4 +1,5 @@
 function solution(date1, date2) {
-    var answer = new Date(date1)<new Date(date2)? 1: 0;
+    // var answer = new Date(date1)<new Date(date2)? 1: 0;
+    var answer = date1.join('') - date2.join('') < 0 ? 1 : 0;
     return answer;
 }
