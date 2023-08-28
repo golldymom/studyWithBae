a6dec9b74a5638e3d56bde8bb50079f1ac3d1d47
golldymom <86226500+golldymom@users.noreply.github.com>
2023-08-28 22:54:44 +0900

    [unrated] Title: 수 조작하기 2, Time: 6.06 ms, Memory: 39.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.js
@@ -0,0 +1,26 @@
+function solution(numLog) {
+    const char={
+         1 : "w",
+        '-1':"s",
+        10:"d"  ,
+        '-10': "a" 
+    }
+    var answer = numLog.map((cur, curIdx, originArr) => {
+        if (curIdx === 0) return ""; // 첫 번째 요소는 빈 문자열로 시작
+        const prev = originArr[curIdx - 1];
+        
+        switch (prev - cur) {
+            case -1:
+                return 'w';
+            case 1:
+                return 's';
+            case -10:
+                return 'd';
+            case 10:
+                return 'a';
+            default:
+                return '';
+        }
+    }).join("");
+    return answer;
+}
